ee440971803f5e4f2bb98359e35e198a1c4c8c2f
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-09-25 19:07:37 -0700

    Added a script that returns information about his/her TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""API REQUESTS MODULE"""
+
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    """Returns information about his/her TODO list progress"""
+
+    user_res = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+    )
+    todo_res = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
+    )
+
+    if user_res.status_code == 200 and todo_res.status_code == 200:
+        j_user_data = json.loads(user_res.text)
+        j_todo_data = json.loads(todo_res.text)
+
+        completed_tasks = [t for t in j_todo_data if t["completed"]]
+        len_comptd = len(completed_tasks)
+        len_total = len(j_todo_data)
+
+        print(
+            f"Employee {j_user_data['name']} is done with\
+ tasks({len_comptd}/{len_total}):"
+        )
+
+        for val in completed_tasks:
+            print(
+                f"\
+    {val['title']}"
+            )
