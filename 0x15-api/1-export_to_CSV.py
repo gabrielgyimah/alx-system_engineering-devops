eec903bcba4c05fc0358a1c1cc8d790f9de9c58c
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-09-25 19:20:58 -0700

    Added A CSV Storage Module

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""CSV Storage Module"""
+
+from sys import argv
+import csv
+import requests
+
+
+if __name__ == "__main__":
+    """Stores TODO list progress to a csv file."""
+    base_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    todo_url = '{}/todos'.format(base_url)
+    res = requests.get(todo_url)
+    if res.status_code == 200:
+        res = res.json()
+        name = requests.get(base_url).json().get('username')
+        filename = "{}.csv".format(argv[1])
+        with open(filename, 'w', encoding='utf8') as csv_file:
+            csv_file = csv.writer(
+                    csv_file, delimiter=',',
+                    quoting=csv.QUOTE_ALL, quotechar='"')
+            for task in res:
+                csv_file.writerow(
+                        [
+                            task['userId'], name,
+                            task['completed'], task['title']])
