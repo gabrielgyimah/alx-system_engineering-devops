ec5cf31fbd9cd9d5ebcdac6a04ec5ffccbfb43d9
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-10-10 10:22:17 -0700

    Added a script that  queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""10 Hot Posts Listed Module"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(
+        url,
+        headers={'User-Agent': 'Mozilla/5.0'},
+        allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"][:10]
+        for post in posts:
+            print(post["data"]["title"])
+    else:
+        print("None")
